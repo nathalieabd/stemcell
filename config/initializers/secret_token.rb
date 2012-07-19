b02d8c50d0b49389991e75fac4738e8304f46e37
Kevin J. Dickerson <kevin.dickerson@gmail.com>
2012-07-19 15:38:15 -0700

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Stemcell::Application.config.secret_token = 'e0c142d0d4b6f00abbe637cb8ff570a98cf47eee9dd933600b41d82358699464b37df1ed704525b68903aa96a3ac323bb130e704e38f4da146938c998f85d6bf'
