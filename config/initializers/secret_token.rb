345da1631a046492b8a1378e7f4c4c79c66a0855
Kevin J. Dickerson <kevin.dickerson@gmail.com>
2012-07-21 00:59:45 -0700

    Defaults for rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stemcell::Application.config.secret_token = '43962c9accf701aaec361c1ea6cb37df7fa7cb8688279e7dd10aac2cd2220bc693102577f1d7e1033f286f36fa76577840eb5d19fb7959d3e4acea677c4e45a3'
