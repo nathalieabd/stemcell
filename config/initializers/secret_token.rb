e9d7692d934a76a26e0a320c1aceb796bbbfab25
Kevin J. Dickerson <kevin.dickerson@gmail.com>
2012-07-23 20:14:49 -0700

    Initial backbone with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stemcell::Application.config.secret_token = '76d271098c8069da90f6de8d267038076fb3d0eb3c4503326284fae144a9bbca1bcebdb39d281dffefb30933a27d5caf513514209474809c93a49d1080c11674'
