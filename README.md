774632d7323abcf0d58293e4e584347853188b9e
Kevin J. Dickerson <kevin.dickerson@gmail.com>
2012-07-30 20:33:17 -0700

    Initial Rails app.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-stemcell
-========
-
-Stem cell.
+# Nothing here
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stemcell::Application.config.secret_token = 'ba7cb67a8f66ca5b1f93e9db9349f0327369197f492f0ea48b50008e91fdfb00532b8a81b1d4510951c6ab5a83ad7f6e9d4b7d3567e889c8f1f4290c609db50c'
